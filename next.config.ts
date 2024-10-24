6252914c3ca696d08c10f9a26c10bef9ecb5ccbe
yashty <yashtyagi2612@gmail.com>
2024-10-24 17:10:39 +0530

    added home page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
